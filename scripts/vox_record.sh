bb8688ad3db2f59949297a918f49a4efe108853e
b08x <rwpannick@gmail.com>
2020-06-08 04:11:52 -0400

    adding script to start timemachine and connect to handheld mic

--- scripts/vox_record.sh
+++ scripts/vox_record.sh
@@ -3,4 +3,23 @@
 
 RECORDINGS="$HOME/Storage/recordings/thinkings"
 
-timemachine -c 1 -t 30 :q!@
+trap "exit" INT TERM ERR
+trap "kill 0" EXIT
+#if already started, this command will yield an error
+#complaining that it's already being commanded to do stuff
+#which should be ok because using the backslash between these two commands will allow both commands to run regardless if the first one fails. Otherwise, using "&&" would prevent timemachine from opening as those symbols indicate that any command after the first one will not run if the first one fails.
+zita-a2j -j powermic -d hw:PowerMicIINS -c 1 & \
+ZITA_PID="$!"
+
+
+sleep 1
+
+timemachine -t 30 powermic:capture_1 -f wav -s -p "$RECORDINGS/tm-" &
+
+wait
+
+
+
+
+#ideally, when closing timemachine, the zita process would also stop. as it stands, which is an odd way of explaining current circumstances, the zita prcess remains running in the background after timemachine is closed.
+#I'm not sure if I care about this? Well, if I had the time to be particular, sure this is a nice detail.
